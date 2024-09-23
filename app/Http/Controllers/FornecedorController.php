7870ce963a9cc9040378d5ca3b8c5fc96d0c6f82
lucas <lucasgarciaamorim@gmail.com>
2024-09-23 19:36:52 -0300

    48 - Blade - @unless

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
-        return view('app.fornecedor.index');
+    public function index()
+    {
+
+        $fornecedores = [0 => ['name' => 'fornecedor 1', 'status' => 'N']];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,18 +1,22 @@
 <h3>Fornecedor</h3>
 
-{{ 'Texto de teste'}}
-
-<?= 'Texto de teste '?>
-
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
-
 @php
 
-//Para comentários de uma linha
-/*
-Para comentários de multiplas linhas
-*/
-
-echo 'Texto de teste no bloco php '
-
 @endphp
+
+
+
+/*
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3> Existem alguns fonecedores cadastrados </h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem vários fornecedores cadastrados</h3>
+@endif
+
+
+@unless ($fornecedores[0]['status'] == 'S')
+Fornecedor Inativo
+
+@endunless
